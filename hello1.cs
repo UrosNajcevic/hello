4d7c7106f4d310e31c446daba98c8207a5f53992
UrosNajcevic <138773947+UrosNajcevic@users.noreply.github.com>
2023-07-07 12:05:53 +0200

    Add files via upload

---
+++ hello1.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Zdravo!") ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Gahaha!");
+    }
+}
